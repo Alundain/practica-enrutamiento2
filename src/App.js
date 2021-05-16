78c291a6363027f3f75a6b1cbf2cdc73815dce44
Dorian Donoso Ampuero <adonosoaster@gmail.com>
2021-05-15 21:39:14 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Colors from "./components/Colors";
+import Four from "./components/Four";
+import Hello from "./components/Hello";
+import Home2 from "./components/Home2";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container mt-5">
+        <div className="btn-group">
+          <Link to="/home" className="btn btn-dark">
+           Home
+          </Link>
+          <Link to="/4" className="btn btn-dark">
+            Four
+          </Link>
+          <Link to="/hello" className="btn btn-dark">
+            Hello
+          </Link>
+          <Link to="/blue/red" className="btn btn-dark">
+            Colors
+          </Link>
+        </div>
+      <Switch>
+        <Route path="/home">
+         <Home2/>
+        </Route>
+        <Route path="/4">
+          <Four/>
+        </Route>
+        <Route path="/hello">
+          <h2>The world is : hello</h2>
+        </Route>
+        <Route path="/blue/red">
+         <Colors/>
+        </Route>
+        <Route path="/">
+          <Hello/>
+        </Route>
+      </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
---
+++ src/components/Colors.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Colors = () => {
+    return (
+        <div>
+             <h2>The world is : hello</h2>
+        </div>
+    )
+}
+
+export default Colors
